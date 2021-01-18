51ebadd6ddb700af583d873e8f8d8e9f6ec31936
hortiturkey <45897465+hortiturkey@users.noreply.github.com>
2021-01-18 17:34:07 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,28 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDgmhbNcQv95A2HvW2LnMQWJuVrA7n0xGo",
+    authDomain: "hortiturkey-b9816.firebaseapp.com",
+    projectId: "hortiturkey-b9816",
+    storageBucket: "hortiturkey-b9816.appspot.com",
+    messagingSenderId: "182743069045",
+    appId: "1:182743069045:web:aaebd8839e3e9e5943233c"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var AuthPromise = new Promise(function (resolve, reject) {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log('Already logged in.');
+            resolve(true)
+        } else {
+
+            firebase.auth().signInAnonymously().then(() => {
+                console.log('Logged in anonymously.')
+                resolve(true)
+            })
+
+        }
+    });
+
+});
