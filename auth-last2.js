bcbd47d2d45214965101ddf8463807dab5c2c85b
hortiturkey <45897465+hortiturkey@users.noreply.github.com>
2021-03-11 23:19:46 +0300

    Create auth-last2.js

---
+++ auth-last2.js
@@ -0,0 +1,32 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDgmhbNcQv95A2HvW2LnMQWJuVrA7n0xGo",
+    authDomain: "hortiturkey-b9816.firebaseapp.com",
+    databaseURL: "https://hortiturkey-b9816-default-rtdb.firebaseio.com",
+    projectId: "hortiturkey-b9816",
+    storageBucket: "hortiturkey-b9816.appspot.com",
+    messagingSenderId: "182743069045",
+    appId: "1:182743069045:web:aaebd8839e3e9e5943233c"
+    
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var AuthPromise = new Promise(function (resolve, reject) {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log('Already logged in.');
+            resolve(true)
+        } else {
+
+            firebase.auth().signInAnonymously().then(() => {
+                console.log('Logged in anonymously.')
+                resolve(true)
+            })
+
+        }
+    });
+
+});
+
+var database = firebase.database();
